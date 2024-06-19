ed511811c9fb133524407f77343b36956d734eb6
Clifford Ouma <ochiengclifford2000@gmail.com>
2024-06-19 05:23:21 +0300

    Add nullable property

--- SIL-Technical-Interview/Models/RicknMortyCharacter.cs
+++ SIL-Technical-Interview/Models/RicknMortyCharacter.cs
@@ -1,27 +1,27 @@
 public class RicknMortyCharacter {
 	//create properties for the character
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Status { get; set; }
-    public string Species { get; set; }
-    public string Type { get; set; }
-    public string Gender { get; set; }
-    public Origin Origin { get; set; }
-    public Location Location { get; set; }
-    public string Image { get; set; }
+    public string? Name { get; set; }
+    public string? Status { get; set; }
+    public string? Species { get; set; }
+    public string? Type { get; set; }
+    public string? Gender { get; set; }
+    public Origin? Origin { get; set; }
+    public Location? Location { get; set; }
+    public string? Image { get; set; }
     
 }
 
 public class Location
 {
-    public string Name { get; set; }
-    public string Url { get; set; }
+    public string? Name { get; set; }
+    public string? Url { get; set; }
 }
 
 public class Origin
 {
-    public string Name { get; set; }
-    public string Url { get; set; }
+    public string? Name { get; set; }
+    public string? Url { get; set; }
 }
 
 
